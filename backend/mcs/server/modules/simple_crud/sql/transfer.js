97704718ca8753b7e734a39d5729baa8a52bddf0
chhatkulikapilronaldo <chhatkulikapil997@gmail.com>
2023-07-03 10:59:49 +0545

    transfer complet

--- backend/mcs/server/modules/simple_crud/sql/transfer.js
+++ backend/mcs/server/modules/simple_crud/sql/transfer.js
@@ -5,7 +5,7 @@ const helpers = require("../../../helpers");
   const { dbHelper } = require("../../../helpers");
   const httpStatus = require("http-status");
 
-  module.exports = async (call) => {
+  module.exports = async call => {
     let connection;
     try {
       let response = {
@@ -26,65 +26,70 @@ const helpers = require("../../../helpers");
       const Sender_Account = userdetail1[0][0].Account_Number;
       const PinVerify = userdetail1[0][0].PIN;
 
-      const insert = {
-        Sender_Account: Sender_Account,
-        Receiver_Account: call.Receiver_Account,
-        Amount: call.Amount,
-        Remarks: call.Remarks,
-      };
+      const insert ={
+        Sender_Account:Sender_Account,
+        Receiver_Account:call.Receiver_Account,
+        Amount:call.Amount,
+        Remarks:call.Remarks
+      }
 
-      const [Receiver_Account] = await connection.query(
-        `Select * from users where Account_Number='${call.Receiver_Account}'`
-      );
+      const [Receiver_Account] = await connection.query(`Select * from users where Account_Number='${call.Receiver_Account}'`);
       const receive_info = Receiver_Account[0].uuid;
       const receive_user_id = Receiver_Account[0].id;
-      const value = {
-        uuid: receive_info,
-        Account_Number: call.Receiver_Account,
-        Amount: call.Amount,
-        Remarks: call.Remarks,
-        user_id: receive_user_id,
-      };
-      if (pin == PinVerify) {
-        connection = await dbHelper.getConnection();
-        await connection.query(
-          `Update deposits set Amount = Amount - ? where Account_Number = ? LIMIT 1;`,
-          [insert.Amount, insert.Sender_Account],
-          (err, result) => {
-            if (err) {
-              connection.rollback(() => {
+      const value ={
+        uuid:receive_info,
+        Account_Number:call.Receiver_Account,
+        Amount:call.Amount,
+        Remarks:call.Remarks,
+        user_id:receive_user_id
+      }
+      connection = await dbHelper.getConnection();
+      let sql1 = `SELECT u.FullName AS Account_Holder, u.Account_Number, sum(d.Amount) as Total_Amount 
+      from Users As u
+      INNER JOIN 
+      deposits as d on u.id = d.user_id
+      where u.Account_Number='${insert.Sender_Account}'
+      GROUP BY u.FullName, u.Account_Number
+      LIMIT 0, 2000;`;
+      const [rows1] = await connection.query(sql1);
+      if(insert.Amount <=rows1[0].Total_Amount){
+        if(pin == PinVerify){
+          connection = await dbHelper.getConnection();
+          await connection.query(`Update deposits set Amount = Amount - ? where Account_Number = ? LIMIT 1;`,[insert.Amount,insert.Sender_Account],(err,result)=>{
+            if(err){
+              connection.rollback(()=>{
                 throw err;
               });
             }
-            // Check if sender's account has sufficient balance
-            if (result.affectedRows === 0) {
-              connection.rollback(() => {
-                throw new Error("Insufficient balance.");
-              });
-            }
+  
+          });
+    
+          connection = await dbHelper.getConnection();
+          await connection.query(`Insert into deposits set ?`,value);
+    
+          connection = await dbHelper.getConnection();
+          const [transferAmount] = await connection.query(
+          `insert into transfers set ?`,insert
+          );
+          if(transferAmount.insertId > 0){
+            response.status = true;
+            response.message = "Transfer Successfully";
           }
-        );
-
-        connection = await dbHelper.getConnection();
-        await connection.query(`Insert into deposits set ?`, value);
-
-        connection = await dbHelper.getConnection();
-        const [transferAmount] = await connection.query(
-          `insert into transfers set ?`,
-          insert
-        );
-        if (transferAmount.insertId > 0) {
-          response.status = true;
-          response.message = "Transfer Successfully";
+          return response;
         }
-        return response;
-      } else {
+        else{
+          return response;
+        }
+      }
+      else{
         return response;
       }
+      
+      
     } catch (error) {
       throw error;
     } finally {
       if (connection) dbHelper.release(connection);
     }
   };
-})();
+})()
